86aa2601b6c97f1ac468a187ed8dd91f5f918dd5
Megan Weaver <110687887+Meganweaver87@users.noreply.github.com>
2022-08-20 17:02:38 -0500

    "Third commit. Added code to script and scriptHelper."

--- script.js
+++ script.js
@@ -22,14 +22,27 @@ const pickPlanet = require("./scriptHelper.js");
 window.addEventListener("load", function() {
 
    let listedPlanets;
-   let listedPlanetsResponse;
+   //let listedPlanetsResponse = value returned by calling myfetch, which is a promise;
    listedPlanetsResponse = planetsReturned;
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
-       console.log(listedPlanets);
+         console.log(listedPlanets);
+         let form = document.querySelector("form");
+         form.addEventListener("submit", function() {
+            console.log("submit");
+            let pilotName = document.querySelector("input[name=pilotName]");
+            let copilotName = document.querySelector("input[name=copilotName");
+            let fuelStatus = document.querySelector("input[name=fuelLevel]");
+            let cargoStatus = document.querySelector("input[name=cargoMass]");
+            formSubmission(document, pilotName.value, copilotName.value, fuelStatus.value, cargoStatus.value);
+            preventDefault();
+         }); // close event listener
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+   }); // close then
+
+
+
+ 
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,11 @@
 require('isomorphic-fetch');
 
+// pilotName = document.getElementById("pilotStatus");
+//let copilotName = document.getElementById("copilotStatus");
+//let fuelStatus = document.getElementById("fuelStatus");
+// cargoStatus = document.getElementById("cargoStatus");
+
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
     /*
@@ -13,88 +19,91 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                  </ol>
                  <img src="">
     */
- };
+   // NO RETURN VALUE
+};
  
- function validateInput(testInput) {
-     if (testInput.value === "") {
-         console.log("empty string");
-         window.alert("All fields required! Please try again.");
-         return "Empty";
-     } else if (isNaN(Number(testInput.value))) { // if converting testinput results in NaN
-         console.log("not a number");
-         return "Not a Number";
-     } else if ((Number(testInput.value).type == number)) { // if converting testinput results in number
-         console.log("is a number");
-         return "Is a Number";
-     } else {
-         return "Invalid Input";
-     } // chapter 25.9 gave us a better way to do this??? I need a window load event???
- };
+
+function validateInput(testInput) {
+    if (testInput.value === "") {
+        console.log("empty string");
+        window.alert("All fields required! Please try again.");
+        return "Empty";
+    } else if (isNaN(Number(testInput.value))) { // if converting testinput results in NaN
+        console.log("not a number");
+        return "Not a Number";
+    } else if ((Number(testInput.value).type == number)) { // if converting testinput results in number
+        console.log("is a number");
+        return "Is a Number";
+    } else {
+        return "Invalid Input. Please try again.";
+    } // chapter 25.9 gave us a better way to do this??? I need a window load event???
+};
 
  
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-     document.getElementById("pilotStatus") = pilotStatus;
-     document.getElementById("copilotStatus") = copilotStatus;
-     // why won't it let me do this? Am I reassigning this variable?
-     // will only let me write this without a problem with no "let" in front
-     document.getElementById("fuelStatus") = fuelStatus;
-     document.getElementById("launchStatus") = launchStatus;
-     document.getElementById("visibility") = visibility;
-     document.getElementById("cargoStatus") = cargoStatus;
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+
+    let launchStatus = document.getElementById("launchStatus");
+    let visibility = document.getElementById("visibility");
+
+    if (validateInput(pilotName) === "Not a Number" ) {
+        pilotStatus.innerHTML = `${pilotName} ready!`;
+        console.log("formsubmissionpilot");
+    }
+
+    if (validateInput(pilotName) === "Not a Number") {
+        copilotStatus.innerHTML = `${copilotName} ready!`;
+        console.log("formsubmissioncopilot");
+    }
+     
+    if (validateInput(fuelStatus) === "Is a Number") {
+        if (Number(fuelStatus.innerHTML) < 10000 ) {
+            launchStatus.innerHTML = "Fuel level low. Shuttle not ready for launch.";
+            launchStatus.innerHTML.color = "#de919a";
+            visibility = "visible";
+            console.log("formsubmissionfuel");
+        } // close if
+    }
+
+    if (validateInput(cargoStatus) === "Is a Number") {
+        if (Number(cargoStatus.innerHTML) > 10000 ) {
+            launchStatus.innerHTML = "Cargo level high. Shuttle not ready for launch.";
+            launchStatus.innerHTML.color = "#de919a";
+            visibility = "visible";
+            console.log("formsubmissioncargo");
+        } // close if
+    } 
+
     
-    for (argument in formSubmission) {
-         validateInput(argument);
-     } // close for
-     
-    //   if ( idk ) {
-    //     pilotStatus.innerHTML = `${pilotName} ready!`;
-    //  }
+    return true;
+}; // close formsubmission
 
-    //  if ( idk) {
-    //     copilotStatus.innerHTML = `${copilotName} ready!`;
-    //  }
-     
-     
-     if (Number(fuelStatus.innerHTML) < 10000 ) {
-        launchStatus.innerHTML = "Fuel level low. Shuttle not ready for launch.";
-        launchStatus.innerHTML.color = "#de919a";
-        visibility = "visible";
-     }
-     if (Number(cargoStatus.innerHTML) > 10000 ) {
-        launchStatus.innerHTML = "Cargo level high. Shuttle not ready for launch.";
-        launchStatus.innerHTML.color = "#de919a";
-        // visibility visible
-     }
-     
 
-    // does it need a return value?
- };
+async function myFetch() {
+    let planetsReturned;
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        response.json().then(function(json) {
+            return json;
+        }); // clsoe response
+    }); // close planetsReturned
+}; // close myFetch
+ 
+// DONE
+function pickPlanet(planets) {
+    let selectedPlanet = Math.floor(Math.random() * planets.length);
+    return selectedPlanet;
+}; // close pickPlanet
+ 
 
- // where should my button code go? Don't I need some type of event handler that will tell my code what to do when the button is clicked?
-
- 
- async function myFetch() {
-     let planetsReturned;
- 
-     planetsReturned = await fetch().then( function(response) {
-         });
- 
-     return planetsReturned;
- };
- 
- function pickPlanet(planets) {
- };
- 
- module.exports = {
+module.exports = {
      addDestinationInfo: addDestinationInfo,
      validateInput: validateInput,
      formSubmission: formSubmission,
      pickPlanet: pickPlanet,
      myFetch: myFetch
- };
+}; // close exports
  
- // module.exports.addDestinationInfo = addDestinationInfo;
- // module.exports.validateInput = validateInput;
- // module.exports.formSubmission = formSubmission;
- // module.exports.pickPlanet = pickPlanet; 
- // module.exports.myFetch = myFetch;
+// module.exports.addDestinationInfo = addDestinationInfo;
+// module.exports.validateInput = validateInput;
+// module.exports.formSubmission = formSubmission;
+// module.exports.pickPlanet = pickPlanet; 
+// module.exports.myFetch = myFetch;
